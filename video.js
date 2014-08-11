fcfdb9c0279018c3da5830735f1090843ca166b7
Merwan Achibet <achibetmerwan@gmail.com>
2014-08-11 18:31:55 +0200

    - add basic webgl rendering

--- video.js
+++ video.js
@@ -5,12 +5,19 @@ X.Video = (function() {
   'use strict';
 
   var canvas;
+  var buffer;
 
   var colors = [
     [0xFF, 0xFF, 0xFF, 0xFF],
-   	[0xAA, 0xAA, 0xAA, 0xFF],
+    [0xAA, 0xAA, 0xAA, 0xFF],
     [0x66, 0x66, 0x66, 0xFF],
     [0x00, 0x00, 0x00, 0xFF]
+  ];
+  colors = [
+    [1, 1, 1, 1],
+    [0.7, 0.7, 0.7, 1],
+    [0.4, 0.4, 0.4, 1],
+    [0, 0, 0, 1]
   ];
   var color_transparent = [0, 0, 0, 0];
 
@@ -146,12 +153,12 @@ X.Video = (function() {
 		  tile_data = new Array(6144);
 		  oam = new Array(160);
 
+      X.Renderer.init();
+
 		  X.Utils.fill(background_maps);
 		  X.Utils.fill(tile_data);
 		  X.Utils.fill(oam);
-
-    	canvas = document.querySelector('section#game canvas').getContext('2d');
-
+      
       for (var t = 0, l = this.cached_tiles.length; t < l; ++t) {
       	var pixels = new Array(64);
       	X.Utils.fill(pixels);
@@ -174,6 +181,8 @@ X.Video = (function() {
 
     reset: function() {
 
+      X.Renderer.reset();
+
 		  X.Utils.fill(background_maps);
 		  X.Utils.fill(tile_data);
 		  X.Utils.fill(oam);
@@ -181,12 +190,56 @@ X.Video = (function() {
     	this.mode = 2; // Really necessary??
     },
 
-    draw_frame: function() {
+    draw_pixels: function() {
 
     	if (!this.display_enable)
     		return;
 
-      canvas.clearRect(0, 0, 160, 144);
+      canvas.clear(canvas.COLOR_BUFFER_BIT | canvas.DEPTH_BUFFER_BIT);
+
+      if (this.bg_enable) {
+
+        for (var dy = 0; dy <= 18; ++dy) {
+          for (var dx = 0; dx <= 20; ++dx) {
+
+            var tx = dx;
+            var ty = dy;
+
+            var tile_number = X.Memory.r(this.bg_tile_map + ty*32 + tx);
+            tile_number = this.bg_window_tile_data == 0x8000 ? tile_number : 256 + X.Utils.signed(tile_number);
+            var tile = this.cached_tiles[tile_number];
+
+
+            for (var y = 0; y < 8; ++y) {
+              for (var x = 0; x < 8; ++x) {
+
+                var px = dx*8 + x;
+                var py = dy*8 + y;
+
+                var index = py*160*6*6 + px*6*6;
+
+                var c = this.cached_bg_colors[tile[y*8 + x]];
+                var r = c[0];
+                var g = c[1];
+                var b = c[2];
+                var a = c[3]; 
+
+                buffer[index] = px; buffer[index + 1] = py; buffer[index + 2] = r; buffer[index + 3] = g; buffer[index + 4] = b; buffer[index + 5] = a;
+                buffer[index + 6] = px+1; buffer[index + 7] = py; buffer[index + 8] = r; buffer[index + 9] = g; buffer[index + 10] = b; buffer[index + 11] = a;
+                buffer[index + 12] = px+1; buffer[index + 13] = py+1; buffer[index + 14] = r; buffer[index + 15] = g; buffer[index + 16] = b; buffer[index + 17] = a;
+                buffer[index + 18] = px; buffer[index + 19] = py+1; buffer[index + 20] = r; buffer[index + 21] = g; buffer[index + 22] = b; buffer[index + 23] = a;
+                buffer[index + 24] = px+1; buffer[index + 25] = py+1; buffer[index + 26] = r; buffer[index + 27] = g; buffer[index + 28] = b; buffer[index + 29] = a;
+                buffer[index + 30] = px; buffer[index + 31] = py; buffer[index + 32] = r; buffer[index + 33] = g; buffer[index + 34] = b; buffer[index + 35] = a;
+              }
+            }
+          }
+        }
+
+        canvas.bufferData(canvas.ARRAY_BUFFER, buffer, canvas.STATIC_DRAW);
+        canvas.drawArrays(canvas.TRIANGLES, 0, buffer.length/6);
+      }
+
+      /*canvas.clearRect(0, 0, 160, 144);
       
     	// Background
 
@@ -207,18 +260,21 @@ X.Video = (function() {
 	          var tile_number = X.Memory.r(this.bg_tile_map + ty*32 + tx);
 	          tile_number = this.bg_window_tile_data == 0x8000 ? tile_number : 256 + X.Utils.signed(tile_number);
 	          
-	          var tile = canvas.createImageData(8, 8);
+	          /*var tile = canvas.createImageData(8, 8);
 		        X.Utils.cache_to_image(this.cached_tiles[tile_number], this.cached_bg_colors, tile.data);
 		        //canvas.putImageData(tile, dx*8-ox, dy*8-oy);
 var c = document.createElement('canvas');
           c.width = c.height = 8;
           c.getContext("2d").putImageData(tile, 0, 0);
-          canvas.drawImage(c, dx*8-ox, dy*8-oy);
-        }
-			}
+          canvas.drawImage(c, dx*8-ox, dy*8-oy);*/
+
+
+//        }
+
+	//		}
 
       // Objects
-
+/*
       if (this.obj_enable) {
       
 	      for (var o = 0; o < 40; ++o) {
@@ -245,7 +301,7 @@ var c = document.createElement('canvas');
           canvas.drawImage(c, pos_x, pos_y);
 	      }
 			}
-
+*/
       // TODO different palettes
       // TODO flip
       // TODO depth priority
@@ -313,4 +369,103 @@ var c = document.createElement('canvas');
   	
   };
   
+})();
+
+
+
+
+X.Renderer = (function() {
+
+  'use strict';
+
+  var canvas;
+  var program;
+
+  var bg_buffer;
+
+  var vertex_shader_string = '\
+    attribute vec2 a_pos;\
+    attribute vec4 a_col;\
+    uniform vec2 u_res;\
+    varying vec4 v_col;\
+    void main() {\
+      vec2 p = a_pos / u_res * 2.0 - 1.0;\
+      gl_Position = vec4(p, 0.0, 1.0) * vec4(1, -1, 1, 1);\
+      v_col = a_col;\
+    }';
+
+  var fragment_shader_string = '\
+    precision mediump float;\
+    varying vec4 v_col;\
+    void main() {\
+      gl_FragColor = v_col;\
+    }';
+      
+  var createProgram = function(gl, vs, fs) {
+
+    var program = gl.createProgram();
+    
+    var vertex_shader = createShader(gl, vs, gl.VERTEX_SHADER);
+    var fragment_shader = createShader(gl, fs, gl.FRAGMENT_SHADER);
+    gl.attachShader(program, vertex_shader);
+    gl.attachShader(program, fragment_shader);
+    
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+      console.log(gl.getProgramInfoLog(program));
+
+    return program;
+  };
+
+  var createShader = function(gl, shader_string, type) {
+
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, shader_string);
+
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+      console.log(gl.getShaderInfoLog(shader));
+
+    return shader;
+  };
+
+  return {
+
+    init: function() {
+
+      canvas = document.querySelector('section#game canvas').getContext('webgl');
+
+      program = createProgram(canvas, vertex_shader_string, fragment_shader_string);
+      canvas.useProgram(program);
+
+      bg_buffer = new Float32Array(160*144*6*6);
+      
+      var vbo = canvas.createBuffer();
+      canvas.bindBuffer(canvas.ARRAY_BUFFER, vbo);
+      
+      program.pos = canvas.getAttribLocation(program, 'a_pos');
+      canvas.enableVertexAttribArray(program.pos);
+      canvas.vertexAttribPointer(program.pos, 2, canvas.FLOAT, false, 24, 0);
+
+      program.col = canvas.getAttribLocation(program, 'a_col');
+      canvas.enableVertexAttribArray(program.col);
+      canvas.vertexAttribPointer(program.col, 4, canvas.FLOAT, false, 24, 8);
+
+      program.res = canvas.getUniformLocation(program, 'u_res');
+      canvas.uniform2fv(program.res, [160, 144]);
+
+      canvas.clearColor(1, 1, 1, 1);
+    },
+
+    reset: function() {
+
+      // ...
+    },
+
+    draw: function() {
+
+    }
+
+  };
+
 })();
